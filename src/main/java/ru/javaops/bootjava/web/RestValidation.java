0be1ba83caa4feabc6cdda8acfdc74cd62600d0a
sergr972 <sergr972@gmail.com>
2024-01-23 21:55:33 +0300

    add app

---
+++ src/main/java/ru/javaops/bootjava/web/RestValidation.java
@@ -0,0 +1,24 @@
+package ru.javaops.bootjava.web;
+
+import lombok.experimental.UtilityClass;
+import ru.javaops.bootjava.HasId;
+import ru.javaops.bootjava.error.IllegalRequestDataException;
+
+@UtilityClass
+public class RestValidation {
+
+    public static void checkNew(HasId bean) {
+        if (!bean.isNew()) {
+            throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must be new (id=null)");
+        }
+    }
+
+    //  Conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
+    public static void assureIdConsistent(HasId bean, int id) {
+        if (bean.isNew()) {
+            bean.setId(id);
+        } else if (bean.id() != id) {
+            throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must has id=" + id);
+        }
+    }
+}
